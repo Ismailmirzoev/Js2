8ea1a8318588c09b93b4d400ff17cd6190a47b47
Ismailmirzoev <mirzoevismail391@gmail.com>
2025-02-22 18:22:31 +0400

    message

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const name = "Ismayil"; 
-console.log(name);
+setInterval(func1, 1000);
+
+let d = 0;
+function func1() {
+  document.getElementById("id1").innerHTML = d++;
+}
+let i = 2;
+function func2() {
+    let ol = document.getElementById("ol");
+    let l1 = document.createElement("li");
+    l1.innerHTML = "list" + i++;
+    document.body.append(l1);
+    ol.body.append(l1);
+}
---
+++ script2.js
@@ -0,0 +1,3 @@
+function func1() {
+    window.location.href = "http://127.0.0.1:5500/index.html";
+}
