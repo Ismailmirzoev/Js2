16b4c9d12901d8d0bad594a61e0c6b8c300fdf83
Ismailmirzoev <mirzoevismail391@gmail.com>
2025-03-14 19:35:27 +0400

    m

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-let btn = document.getElementById("btn");
-btn.addEventListener("click", func1);
-let inp1 = document.getElementById("inp1");
-let inp2 = document.getElementById("inp2");
+let user = document.getElementById("username");
+let pass = document.getElementById("password");
 function func1() {
-    let parent = document.getElementById("parent");
-    let child = document.getElementById("child");
-    parent.style.display = "flex";
-    child.style.display = "block";
-    localStorage.clear();
-    localStorage.setItem("name", inp1.value);
-    localStorage.setItem("otziv", inp2.value);
-}
-function func2() {
-    window.location.href = "index2.html";
+    window.location.href = "index2.html"
+    let obj = {
+        user: user.value,
+        pass: pass.value
+    }
+    let json1 = JSON.stringify(obj);
+    localStorage.setItem("user", json1);
 }
--- script2.js
+++ script2.js
@@ -1,4 +1,9 @@
-let p = document.getElementById("p");
-let inp1 = localStorage.getItem("name");
-let inp2 = localStorage.getItem("otziv");
-p.innerHTML = `Your Name Is ${inp1} And Your Review Is ${inp2}`;
+let user = document.getElementById("Username");
+let pass = document.getElementById("Password");
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+function func1() {
+    let div = JSON.parse(localStorage.getItem("user"));
+    p1.innerHTML = div.user;
+    p2.innerHTML = div.pass;
+}
